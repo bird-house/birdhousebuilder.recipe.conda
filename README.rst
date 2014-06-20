13a5735086f7107dd5a889cc8214347f8d91b2e6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-20 13:00:04 +0200

    initial project setup

---
+++ README.rst
@@ -0,0 +1,1 @@
+This is a buildout recipe to install anaconda packages.
---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.recipe.cmd
+"""
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.1'
+description = 'A Buildout recipe to install anaconda packages'
+long_description = (
+    open('README.rst').read() + '\n' +
+    open('AUTHORS.rst').read() + '\n' +
+    open('CHANGES.rst').read()
+)
+
+entry_point = 'birdhousebuilder.recipe.conda'
+entry_points = {"zc.buildout": [
+                            "default = %s:Cmd" % entry_point,
+                            "sh = %s:Cmd" % entry_point,
+                            "py = %s:Python" % entry_point,
+                          ],
+                "zc.buildout.uninstall": [
+                            "default = %s:uninstallCmd" % entry_point,
+                            "sh = %s:uninstallCmd" % entry_point,
+                          ],
+                       }
+
+tests_require = ['zope.testing', 'zc.buildout', 'manuel']
+
+setup(name='buildhousebuilder.recipe.conda',
+      version=version,
+      description=description,
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Framework :: Buildout',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'License :: OSI Approved :: BSD License',
+      ],
+      keywords='buildout recipe',
+      author='Carsten Ehbrecht',
+      author_email='ehbrecht@dkrz.de',
+      url='http://www.dkrz.de',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['birdhousebuilder', 'birdhousebuilder.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='birdhousebuilder.recipe.conda.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
