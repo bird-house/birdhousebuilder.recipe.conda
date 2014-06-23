4594a701e88ab04a8abba2910812ae13e852141c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-06-23 12:38:54 +0200

    using buildout options anaconda-home and conda-channel

--- birdhousebuilder/recipe/conda/__init__.py
+++ birdhousebuilder/recipe/conda/__init__.py
@@ -8,8 +8,9 @@ def as_bool(value):
         return True
     return False
 
-def install_pkgs(pkgs):
+def install_pkgs(home, pkgs, channel=None):
     from subprocess import check_call
+    import os
     
     pkgs = pkgs.strip()
     if not pkgs:
@@ -18,13 +19,22 @@ def install_pkgs(pkgs):
         pkg_list = pkgs.split(' ')
         pkg_list = [l.strip() for l in pkg_list]
         for pkg in pkg_list:
-            check_call('/opt/anaconda/bin/conda install -c https://conda.binstar.org/PinguCarsti %s' % (pkg), shell=True)
+            cmd = [os.path.join(home, 'bin/conda')]
+            cmd.append('install')
+            if channel is not None:
+                cmd.append('-c')
+                cmd.append(channel)
+            cmd.append(pkg)
+            check_call(cmd, shell=False)
         
 class Conda(object):
     """This recipe is used by zc.buildout"""
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
+        b_options = buildout['buildout']
+        self.anaconda_home = b_options.get('anaconda-home', '/opt/anaconda')
+        self.conda_channel = b_options.get('conda-channel')
         self.on_install = as_bool(options.get('on_install', 'false'))
         self.on_update = as_bool(options.get('on_update', 'false'))
 
@@ -44,7 +54,7 @@ class Conda(object):
         """run the commands
         """
         pkgs = self.options.get('pkgs', '')
-        install_pkgs(pkgs) 
+        install_pkgs(self.anaconda_home, pkgs, self.conda_channel) 
 
 def uninstall(name, options):
     pass
