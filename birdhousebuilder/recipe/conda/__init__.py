d6ac5f7c61bdd9bfeb52e2e9159e29fea7b72eab
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-28 16:51:17 +0200

    update anaconda home

--- birdhousebuilder/recipe/conda/__init__.py
+++ birdhousebuilder/recipe/conda/__init__.py
@@ -60,7 +60,7 @@ class Recipe(object):
         # buildout option anaconda-home overwrites default anaconda path
         # TODO: do i realy need this?
         # TODO: guess with "which conda"?
-        b_options['anaconda-home'] = b_options.get('anaconda-home', join(os.environ('HOME'), 'anaconda'))
+        b_options['anaconda-home'] = b_options.get('anaconda-home', join(os.environ.get('HOME', '/opt'), 'anaconda'))
 
         # conda prefix
         # either prefix option or conda_env_path (set by conda env) or anaconda_home
